6f6c0675d59e3967d0476f2c13e1b7ba3ef71fb2
T1T1R3 <thiago.souza091202@gmail.com>
2025-05-06 15:44:46 -0300

    disney movies added

--- src/moviesBase.js
+++ src/moviesBase.js
@@ -10,7 +10,6 @@ import Cinderela2 from './moviesCover/Disney/Cinderela2.jpg';
 import Cinderela3 from './moviesCover/Disney/Cinderela3.png';
 import Corcunda from './moviesCover/Disney/Corcunda.webp';
 import Enrolados from './moviesCover/Disney/Enrolados.jpg';
-import Enrolados2 from './moviesCover/Disney/Enrolados2.jpg';
 import Frozen from './moviesCover/Disney/Frozen.jpg';
 import Frozen2 from './moviesCover/Disney/Frozen2.jpg';
 import Moana from './moviesCover/Disney/Moana.jpg';
@@ -33,6 +32,7 @@ import AladdinLive from './moviesCover/Disney/AladdinLive.jpg';
 import PequenaSereiaLive from './moviesCover/Disney/PequenaSereiaLive.jpg';
 import BelaFeraLive from './moviesCover/Disney/BelaFeraLive.jpg';
 import BrancaNeveLive from './moviesCover/Disney/BrancaNeveLive.png';
+import DamaVagabundoLive from './moviesCover/Disney/DamaVagabundoLive.webp'
 
 
 
@@ -57,13 +57,13 @@ export const movies = [
     id: 3,
     title: "A Bela Adormecida",
     image: BelaAdormecida,
-    videoUrl: ""
+    videoUrl: "https://drive.google.com/file/d/1RQHkIdrafa61NCKE4S5iK4w8QT4UI4Az/preview"
   },
   {
     id: 4,
     title: "A Dama e o Vagabundo",
     image: DamaVagabundo,
-    videoUrl: ""
+    videoUrl: "https://drive.google.com/file/d/1w46U5o8hdFUv-q3YOSYAzl7jBVMU5ale/preview"
   },
   // Década de 1990
   {
@@ -77,7 +77,7 @@ export const movies = [
     id: 6,
     title: "A Pequena Sereia II: O Retorno Para o Mar",
     image: PequenaSereia2,
-    videoUrl: ""
+    videoUrl: "https://drive.google.com/file/d/1fWJjBKl4YdMTW8cEu8MKfnvYBd7m72ap/preview"
   },
   {
     id: 7,
@@ -97,13 +97,13 @@ export const movies = [
     id: 9,
     title: "Aladdin",
     image: Alladin,
-    videoUrl: ""
+    videoUrl: "https://drive.google.com/file/d/1pRe6Dzx3FrfumPiX6U-SMsfURjBc8MFd/preview"
   },
   {
     id: 10,
     title: "Aladdin: O Retorno de Jafar",
     image: AladdinJafar,
-    videoUrl: ""
+    videoUrl: "https://drive.google.com/file/d/1SnPVgRZhEP4LmA1pZPSnrG09A4vv31Un/preview"
   },
   {
     id: 11,
@@ -115,7 +115,7 @@ export const movies = [
     id: 12,
     title: "Pocahontas",
     image: Pocahontas,
-    videoUrl: ""
+    videoUrl: "https://drive.google.com/file/d/1Fn_lJZHVC2bd3T1Y-EXyCsW88G0bVsXa/preview"
   },
   {
     id: 13,
@@ -127,26 +127,26 @@ export const movies = [
     id: 14,
     title: "Mulan",
     image: Mulan,
-    videoUrl: ""
+    videoUrl: "https://drive.google.com/file/d/1amkV-cruEy5UN36RGu2rfmsxupqsnmQl/preview"
   },
   {
     id: 15,
     title: "Mulan 2: A Lenda Continua",
     image: Mulan2,
-    videoUrl: ""
+    videoUrl: "https://drive.google.com/file/d/1EersTjVx747twnZkqIjQailYDX1NChOb/preview"
   },
   // Década de 2000
   {
     id: 16,
     title: "Cinderela II: Os Sonhos se Realizam",
     image: Cinderela2,
-    videoUrl: ""
+    videoUrl: "https://drive.google.com/file/d/1pzfDhnChj-7DaW3oZN0V42gK8ZRzs3z5/preview"
   },
   {
     id: 17,
     title: "Cinderela III: Uma Volta no Tempo",
     image: Cinderela3,
-    videoUrl: ""
+    videoUrl: "https://drive.google.com/file/d/1Sz4o32FQp3sCpS3-mU5QPZ9KlYzNUAIi/preview"
   },
   {
     id: 18,
@@ -160,7 +160,7 @@ export const movies = [
     id: 19,
     title: "Enrolados",
     image: Enrolados,
-    videoUrl: ""
+    videoUrl: "https://drive.google.com/file/d/1X9RCIBLmLKib6k-V06IMJ-6uInCLaTGT/preview"
   },
   {
     id: 21,
@@ -174,84 +174,90 @@ export const movies = [
     id: 22,
     title: "Frozen: Uma Aventura Congelante",
     image: Frozen,
-    videoUrl: ""
+    videoUrl: "https://drive.google.com/file/d/1nESTmnXm-o7X7OFfkXFPdgyJXpEqVDBu/preview"
   },
   {
     id: 23,
     title: "Frozen II",
     image: Frozen2,
-    videoUrl: ""
+    videoUrl: "https://drive.google.com/file/d/1RuS9orBKFfo7mHigmTLBtEZooVIGuT__/preview"
   },
   {
     id: 24,
     title: "Moana: Um Mar de Aventuras",
     image: Moana,
-    videoUrl: ""
+    videoUrl: "https://drive.google.com/file/d/1EJClyOGHsaRFdELFpKyJbuoJT9lLprzd/preview"
   },
   // Década de 2020
   {
     id: 25,
     title: "Raya e o Último Dragão",
     image: Raya,
-    videoUrl: ""
+    videoUrl: "https://drive.google.com/file/d/1Gia8TVmeNy1W5mZyaysByuWWfObISqqN/preview"
   },
   {
     id: 26,
     title: "Moana 2",
     image: Moana2,
-    videoUrl: ""
+    videoUrl: "https://drive.google.com/file/d/1QOsJsk01nfmtkm8Jin2xxapAUs22nDyI/preview"
   },
   // Live-actions
   {
     id: 27,
     title: "Cinderela (Live-action)",
     image: CinderelaLive,
-    videoUrl: ""
+    videoUrl: "https://drive.google.com/file/d/1l7ftGyZykcmmLnH-HTSWxcy43lRE9HXL/preview"
   },
   {
     id: 28,
     title: "A Bela e a Fera (Live-action)",
     image: BelaFeraLive,
-    videoUrl: ""
+    videoUrl: "https://drive.google.com/file/d/1AQMiUKjsUn4zHOv7R_vNLDElJvcsmyZ1/preview"
   },
   {
     id: 29,
     title: "Aladdin (Live-action)",
     image: AladdinLive,
-    videoUrl: ""
+    videoUrl: "https://drive.google.com/file/d/1GnAPHUGIAZ-QeabgopmNvLt3MZaiVMHr/preview"
   },
   {
     id: 30,
     title: "Mulan (Live-action)",
     image: MulanLive,
-    videoUrl: ""
+    videoUrl: "https://drive.google.com/file/d/16rTxD4GWwKtLABl0xyUQoLKrGSKvuK7H/preview"
   },
   {
     id: 31,
     title: "A Pequena Sereia (Live-action)",
     image: PequenaSereiaLive,
-    videoUrl: ""
+    videoUrl: "https://drive.google.com/file/d/1ehme0aXTafwDoeUrTX3eoxaDv0Hr3Czs/preview"
   },
   {
     id: 32,
     title: "Branca de Neve (Live-action)",
     image: BrancaNeveLive,
-    videoUrl: ""
+    videoUrl: "https://drive.google.com/file/d/1s03zYMrIn-tPuX58uVZNJy6HoSfw8Biu/preview"
   },
   {
     id: 33,
     title: "Malévola",
     image: Malevola,
-    videoUrl: ""
+    videoUrl: "https://drive.google.com/file/d/1V5JwCK4VkxazWyWcUPU5yxfhoqtLw2ns/preview"
   },
   {
     id: 34,
     title: "Malévola: Dona do Mal",
     image: Malevola2,
-    videoUrl: ""
+    videoUrl: "https://drive.google.com/file/d/1VW7PLtMKYVUu8s-eJLpa9egjv4LoWk-P/preview"
   },
   {
     id: 35,
+    title: "Dama e o Vagabundo (Live-action)",
+    image: DamaVagabundoLive,
+    videoUrl: "https://drive.google.com/file/d/1A2ULzyyU9jLr_5rQVJhPbop_HZSnxs-I/preview"
+  },
+  {
+    id: 36,
     title: "Corcunda de Notre Dame",
     image: Corcunda,
     videoUrl:

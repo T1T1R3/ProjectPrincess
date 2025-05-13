ccb5310b2a07b1b95843c5529c6d9dd6624185c5
T1T1R3 <thiago.souza091202@gmail.com>
2025-05-13 16:36:00 -0300

    ghibli movies cover added

--- src/moviesBase3.js
+++ src/moviesBase3.js
@@ -1,11 +1,182 @@
 import CasteloAnimado from "./moviesCover/Ghibli/CasteloAnimado.webp";
+import CasteloCeu from "./moviesCover/Ghibli/ceu.webp";
+import MeuAmigoTotoro from "./moviesCover/Ghibli/totoro.webp";
+import TumuloVagalumes from "./moviesCover/Ghibli/vagalumes.webp";
+import Nausicaa from "./moviesCover/Ghibli/nausicaa.webp";
+import Kiki from "./moviesCover/Ghibli/kiki.webp";
+import Memorias from "./moviesCover/Ghibli/memorias.webp" ;
+import Porco from "./moviesCover/Ghibli/porco.webp";
+import Oceano from "./moviesCover/Ghibli/ocean.webp";
+import PomPoko from "./moviesCover/Ghibli/pompoko.webp";
+import Sussurros from "./moviesCover/Ghibli/sussurro.webp";
+import Mononoke from "./moviesCover/Ghibli/mononoke.webp";
+import Yamadas from "./moviesCover/Ghibli/yamadas.webp";
+import Chihiro from "./moviesCover/Ghibli/chichiro.webp";
+import Gatos from "./moviesCover/Ghibli/gatos.webp";
+import Terramar from "./moviesCover/Ghibli/terramar.webp";
+import Ponyo from "./moviesCover/Ghibli/ponyo.webp";
+import Pequeninos from "./moviesCover/Ghibli/pequeninos.webp";
+import Kokuriko from "./moviesCover/Ghibli/kokoriko.webp";
+import Vento from "./moviesCover/Ghibli/vento.webp";
+import Kaguya from "./moviesCover/Ghibli/kaguya.webp";
+import Marnie from "./moviesCover/Ghibli/marnie.webp";
+import Bruxa from "./moviesCover/Ghibli/bruxa.webp";
+import Menino from "./moviesCover/Ghibli/menino.webp";
 
 
 export const movies = [
+    // Década de 1980
     {
-        id:1,
-        title:'Castelo Animado (2004)',
-        image:CasteloAnimado,
-        url:''
+      id: 1,
+      title: "Nausicaä do Vale do Vento (1984)",
+      image: Nausicaa,
+      videoUrl: "URL_DO_VIDEO"
+    },
+    {
+      id: 2,
+      title: "O Castelo no Céu (1986)",
+      image: CasteloCeu,
+      videoUrl: "URL_DO_VIDEO"
+    },
+    {
+      id: 3,
+      title: "Meu Amigo Totoro (1988)",
+      image: MeuAmigoTotoro,
+      videoUrl: "URL_DO_VIDEO"
+    },
+    {
+      id: 4,
+      title: "Túmulo dos Vagalumes (1988)",
+      image: TumuloVagalumes,
+      videoUrl: "URL_DO_VIDEO"
+    },
+    {
+      id: 5,
+      title: "O Serviço de Entregas da Kiki (1989)",
+      image: Kiki,
+      videoUrl: "URL_DO_VIDEO"
+    },
+  
+    // Década de 1990
+    {
+      id: 6,
+      title: "Memórias de Ontem (1991)",
+      image: Memorias,
+      videoUrl: "URL_DO_VIDEO"
+    },
+    {
+      id: 7,
+      title: "Porco Rosso: O Último Herói Romântico (1992)",
+      image: Porco,
+      videoUrl: "URL_DO_VIDEO"
+    },
+    {
+      id: 8,
+      title: "Eu Posso Ouvir o Oceano (1993)",
+      image: Oceano,
+      videoUrl: "URL_DO_VIDEO"
+    },
+    {
+      id: 9,
+      title: "PomPoko: A Grande Batalha dos Guaxinins (1994)",
+      image: PomPoko,
+      videoUrl: "URL_DO_VIDEO"
+    },
+    {
+      id: 10,
+      title: "Sussurros do Coração (1995)",
+      image: Sussurros,
+      videoUrl: "URL_DO_VIDEO"
+    },
+  
+    // Década de 2000
+    {
+      id: 11,
+      title: "Princesa Mononoke (1997)",
+      image: Mononoke,
+      videoUrl: "URL_DO_VIDEO"
+    },
+    {
+      id: 12,
+      title: "Meus Vizinhos os Yamadas (1999)",
+      image: Yamadas,
+      videoUrl: "URL_DO_VIDEO"
+    },
+    {
+      id: 13,
+      title: "A Viagem de Chihiro (2001)",
+      image: Chihiro,
+      videoUrl: "URL_DO_VIDEO"
+    },
+    {
+      id: 14,
+      title: "O Reino dos Gatos (2002)",
+      image: Gatos,
+      videoUrl: "URL_DO_VIDEO"
+    },
+    {
+      id: 15,
+      title: "O Castelo Animado (2004)",
+      image: CasteloAnimado,
+      videoUrl: "URL_DO_VIDEO"
+    },
+  
+    // Década de 2010
+    {
+      id: 16,
+      title: "Contos de Terramar (2006)",
+      image: Terramar,
+      videoUrl: "URL_DO_VIDEO"
+    },
+    {
+      id: 17,
+      title: "Ponyo: Uma Amizade Que Veio do Mar (2008)",
+      image: Ponyo,
+      videoUrl: "URL_DO_VIDEO"
+    },
+    {
+      id: 18,
+      title: "O Mundo dos Pequeninos (2010)",
+      image: Pequeninos,
+      videoUrl: "URL_DO_VIDEO"
+    },
+    {
+      id: 19,
+      title: "Da Colina Kokuriko (2011)",
+      image: Kokuriko,
+      videoUrl: "URL_DO_VIDEO"
+    },
+    {
+      id: 20,
+      title: "Vidas ao Vento (2013)",
+      image: Vento,
+      videoUrl: "URL_DO_VIDEO"
+    },
+  
+    // Década de 2020
+    {
+      id: 21,
+      title: "O Conto da Princesa Kaguya (2013)",
+      image: Kaguya,
+      videoUrl: "URL_DO_VIDEO"
+    },
+    {
+      id: 22,
+      title: "As Memórias de Marnie (2014)",
+      image: Marnie,
+      videoUrl: "URL_DO_VIDEO"
+    },
+    {
+      id: 23,
+      title: "Aya e a Bruxa (2020)",
+      image: Bruxa,
+      videoUrl: "URL_DO_VIDEO"
+    },
+    {
+      id: 24,
+      title: "O Menino e a Garça (2023)",
+      image: Menino,
+      videoUrl: "URL_DO_VIDEO"
     }
-]
+  ];
+  
